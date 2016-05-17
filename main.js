57e9e85e02e1d6cc538b66a3ecaf550a9ba9713c
avernus <avernusx@yandex.ru>
2016-05-17 17:41:37 +0300

    EmailList object added. RenderList function added

--- main.js
+++ main.js
@@ -1,6 +1,35 @@
+//объект списка почтовых адресов
+
+function EmailList() {
+
+  	/*this.sortDateASC = function() {
+
+  		var flag = true;
+
+  		while(flag) {
+
+  			this.emailList.forEach(function(email) { 
+
+  			});
+  		}
+  	}*/
+
+  	this.renderList = function() {
+
+  		console.log('render list');
+
+  		this.emailList.forEach(function(email) {
+
+			var emailDiv = document.createElement("div");
+    		emailDiv.innerHTML = "<span>"+email.subject+"</span><br><span>"+email.fromName+"</span>";
+    		document.getElementById("sidebar").appendChild(emailDiv);
+		});
+  	};
+}
+
 //аякс-запрос к бэкенду при загрузке страницы
 
-var emailList = [];
+var myList = new EmailList();
 
 var xhr = new XMLHttpRequest();
 xhr.open('GET', '/emails.json');
@@ -11,17 +40,9 @@ xhr.onreadystatechange = function () {
   	if (xhr.readyState === 4) {
     	if (xhr.status === 200) {
 
-      		var emailList = JSON.parse(xhr.responseText);
-
-      		emailList.forEach(function(email) {
-  				
-  				var emailDiv = document.createElement("div");
-
-        		emailDiv.innerHTML = "<span>"+email.subject+"</span><br><span>"+email.fromName+"</span>";
-
-        		document.getElementById("sidebar").appendChild(emailDiv);
-			});
-
+      		myList.emailList = JSON.parse(xhr.responseText);
+      		myList.renderList();
+      		
     	} else {
       		console.log('Error: ' + xhr.status);
     	}
