24ce19753f50f97b2b2dc074e50675f20d7378ab
avernus <avernusx@yandex.ru>
2016-05-24 11:05:05 +0300

    Завершенный проект без анимаций

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
+function formatDate(time) {
+
+  var dateOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timezone: 'UTC',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
+
+  var date = new Date(time*1000);
+  var currentTime = new Date();
+
+  if((currentTime.getTime() - date.getTime()) < 24*60*60*1000) {
+
+    var dateString = 'сегодня';
+
+  } else if((currentTime.getTime() - date.getTime()) < 24*60*60*1000*2) {
+
+    var dateString = 'вчера';
+
+  } else {
+
+    var dateString = date.toLocaleString("ru", dateOptions);
+  }
+
+  return dateString;
+}
+
 //объект списка почтовых адресов
 
 var emailList = {
@@ -7,8 +38,28 @@ var emailList = {
 
   	showMail: function(index) {
 
-  		document.getElementById("content").innerHTML = this.emailList[index].content;
+  		document.getElementById("email-content").innerHTML = this.emailList[index].content;
+      document.getElementById("email-subject").innerHTML = this.emailList[index].subject;
+      document.getElementById("email-fromName").innerHTML = this.emailList[index].fromName;
+      document.getElementById("email-fromEmail").innerHTML = this.emailList[index].fromEmail;
+      document.getElementById("email-dateReceived").innerHTML = formatDate(this.emailList[index].dateReceived);
   	},
+
+    renderItem: function(email, index) {
+
+      var emailDiv = document.createElement("div");
+      emailDiv.className += "email-item";
+
+      if(!email.read) {
+
+        emailDiv.className += " unread";
+      }
+
+      emailDiv.innerHTML = "<h3>"+email.fromName+"</h3><span class='email-date'>"+formatDate(email.dateReceived)+"</span><br><span>"+email.subject+"</span>";
+
+      emailDiv.onclick = function() { emailList.showMail(index); }
+      document.getElementById("sidebar").appendChild(emailDiv);
+    },
 
   	renderList: function() {
 
@@ -16,32 +67,22 @@ var emailList = {
 
   		document.getElementById("sidebar").innerHTML = "";
 
+      var that = this;
+
   		this.emailList.forEach(function(email, index) {
 
   			if(showUnread) {
 
   				if(!email.read) {
 
-  					var emailDiv = document.createElement("div");
-  					emailDiv.className += "email-item";
-    				emailDiv.innerHTML = "<h3>"+email.fromName+"</h3><span>"+email.subject+"</span>";
-
-    				emailDiv.onclick = function() { emailList.showMail(index); }
-
-    				document.getElementById("sidebar").appendChild(emailDiv);
+  			    that.renderItem(email, index);
   				}
 
   			} else {
 
-  				var emailDiv = document.createElement("div");
-  				emailDiv.className += "email-item";
-    			emailDiv.innerHTML = "<h3>"+email.fromName+"</h3><span>"+email.subject+"</span>";
-
-    			emailDiv.onclick = function() { emailList.showMail(index); }
-
-    			document.getElementById("sidebar").appendChild(emailDiv);
+          that.renderItem(email, index);
   			}
-		});
+		  });
   	},
 
   	switchUnread: function() {
@@ -52,21 +93,28 @@ var emailList = {
 
   	sortDate: function() {
 
-  		if(this.sortDateOrder) {
+  		if(!this.sortDateOrder) {
 
   			this.emailList.sort(function(a, b) {
 		    	return parseFloat(a.dateReceived) - parseFloat(b.dateReceived);
-			});
+			  });
   		} else {
 
   			this.emailList.sort(function(a, b) {
-		    	return parseFloat(a.dateReceived) + parseFloat(b.dateReceived);
-			});
+		    	return parseFloat(b.dateReceived) - parseFloat(a.dateReceived);
+			  });
   		}
 
   		this.sortDateOrder = !this.sortDateOrder;
   		this.renderList();
   	},
+
+    sortDateDesc: function() {
+
+      this.emailList.sort(function(a, b) {
+        return parseFloat(b.dateReceived) - parseFloat(a.dateReceived);
+      });
+    }
 }
 
 //аякс-запрос к бэкенду при загрузке страницы
@@ -81,18 +129,39 @@ xhr.onreadystatechange = function() {
     	if (xhr.status === 200) {
 
       		emailList.emailList = JSON.parse(xhr.responseText);
-      		emailList.renderList();
+      		emailList.sortDateDesc();
+          emailList.renderList();
       		
     	} else {
       		console.log('Error: ' + xhr.status);
     	}
   	}
 };
+darte = new Date();
+console.log(darte.getTime());
 
 //привязка событий
 
 switchUnreadElement = document.getElementById("switchUnread");
-switchUnreadElement.onclick = function() { emailList.switchUnread(); }
+switchUnreadElement.onclick = function() { 
+
+  if(!emailList.showUnread) {
+    switchUnreadElement.innerHTML = 'Показать все';
+  } else {
+    switchUnreadElement.innerHTML = 'Показать непрочитанные';
+  }
+
+  emailList.switchUnread(); 
+}
 
 switchDateSort = document.getElementById("sortDate");
-switchDateSort.onclick = function() { emailList.sortDate(); }
+switchDateSort.onclick = function() { 
+
+  if(!emailList.sortDateOrder) {
+    switchDateSort.innerHTML = 'Сортировать по дате: по убыванию';
+  } else {
+    switchDateSort.innerHTML = 'Сортировать по дате: по возрастанию';
+  }
+
+  emailList.sortDate(); 
+}
